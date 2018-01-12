2ee91a13befbab0f82901b2427a457ccaa1cf486
ydkzheng22 <ydkzheng22@gmail.com>
2018-01-11 22:39:45 -0500

    Add files via upload

---
+++ post-challenges.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+"""
+Author: Freddie Vargus (github.com/FreddieV4)
+File: post_challenges.py
+Purpose: Used to pull weekly challenges from r/dailyprogrammer
+"""
+
+import re
+import os
+import praw
+import logging
+import sys
+
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+		    filename='dpc.log', level=logging.DEBUG)
+
+
+def get_current_week(num_challenges=1):
+	"""
+	Gets 3 challenges, easy, intermediate, hard
+	for the current week from r/dailyprogrammer
+	and stores the challenge text in directories
+	named after the challenge titles
+	"""
+
+	if len(sys.argv) > 1:
+		num_challenges = int(sys.argv[1])
+
+	r = praw.Reddit(user_agent="dailyprogrammer-challenges")
+	sub = r.subreddit("dailyprogrammer")
+
+	# retrieve generators for top posts
+	chals = sub.new(limit=num_challenges)
+	_chals = sub.new(limit=num_challenges)
+
+	# get challenge titles & selftext
+	challenge_titles = [str(x.title) for x in chals]
+	challenge_text = [str(x.selftext) for x in _chals]
+
+	# cleanup titles for directory names
+	title_lst = []
+	logging.info("Started cleaning title names {}".format(challenge_titles))
+	for title in challenge_titles:
+		t = re.sub(r'\[([0-9\-]+)\]', '', title) # removes datestamp
+		t = re.sub(r'[<>:\"\\\/|?*]', '', t) # removes special chars
+		title_lst.append(t.lstrip())
+	logging.info("Finished cleaning title names {}".format(title_lst))
+
+	# name directories after challenges
+	# add challenge selftext to directories
+	logging.info("Started creating directories")
+	for i in range(num_challenges):
+		os.system('mkdir "{}"'.format(title_lst[i]))
+		logging.info("Created directory {}".format(title_lst[i]))
+		f = open('challenge_text.md', 'w')
+		f.write(challenge_text[i])
+		f.close()
+		logging.info("Wrote challenge text to file")
+		os.system('mv challenge_text.md "{}"'.format(title_lst[i]))
+		logging.info("Moved challenge text to directory")
+		# Add a solutions directory to the new challenge directory
+		os.system('mkdir solutions')
+		os.system('mv solutions "{}"'.format(title_lst[i]))
+		logging.info("Created solutions directory")
+
+
+# TODO: Move this to a separate file
+def get_all_challenges():
+	"""
+	Gets all challenges from the entire dailyprogrammer
+	subreddit and stores their titles and selftexts
+	in their own directories
+	"""
+
+	r = praw.Reddit(user_agent="dailyprogrammer-all")
+	sub = r.search("Challenge #", subreddit="dailyprogrammer", sort="hot", limit=1000, period='all')
+	_sub = r.search("Challenge #", subreddit="dailyprogrammer", sort="hot", limit=1000, period='all')
+
+
+	# get challenge titles & selftext
+	challenge_titles = [catch(str(x.title)) for x in sub]
+	challenge_text = [catch(str(x.selftext)) for x in _sub]
+
+	# cleanup titles for directory names
+	title_lst = []
+	for title in challenge_titles:
+		t = re.sub(r'\[([0-9\-\/]+)\]', '', title)
+		t = re.sub(r'[<>:\"\\\/|?*]', '', t)
+		title_lst.append(t.lstrip())
+
+	# name directories after challenges
+	for i in range(len(challenge_titles)):
+		os.system('mkdir "{}"'.format(title_lst[i]))
+
+	# add challenge selftext to directories
+	for i in range(len(challenge_titles)):
+		f = open('challenge_text.md', 'w')
+		f.write(challenge_text[i])
+		f.close()
+		os.system('mv challenge_text.md "{}"'.format(title_lst[i]))
+
+
+def catch(data):
+	"""
+	Used to skip over any encoding errors when
+	creating titles and selftext lists
+	"""
+
+	try:
+		return data
+	except UnicodeEncodeError as e:
+		logging.info('UnicodeEncodeError: ' + e)
+
+
+if __name__ == '__main__':
+	logging.info("Started get_current_week()")
+	get_current_week()
+	logging.info("Finished get_current_week()")
---
+++ transform.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+"""
+Author: Tyler Nivin
+Github: github.com/twn346
+File: transform.py
+Purpose: Used to rename directories to fit a specific naming scheme
+i.e. Challenge 0001 - {challenge title}
+"""
+
+import glob
+import os
+import re
+
+# This script will run on the Easy Medium and Hard directories.
+
+
+def update_challenge_dir_name(oldName, difficulty):
+    '''Update the specified challenge dirs names. Returns original name if it
+    suspects that it would fail a valid transformation.'''
+
+    # Find the challenge number:
+    # tokens, delimited by ' '
+    # find the first token that has one number embedded within
+    # This should be the challenge number
+    # This should avoid other strings with multiple numbers e.g. '[2016-05-20]'
+    tokens = oldName.split()
+
+    currNum = ''
+    n = 0
+    while (len(tokens) > 0 and n == 0):
+        a_token = tokens.pop(0)
+        match = re.findall(r'(\d{1,5})', a_token)
+        if (match and len(match) == 1):
+            currNum = match[0]
+            n = len(currNum)
+
+    # Clean up submissions with leading zeros
+    if (a_token[0] == '#' and n != 0 and currNum[0] == '0'):
+        currNum = currNum[1:]
+        n = n - 1
+
+    # No correct looking number found, or, it appears that the
+    # number returned already appears to be in canonical form
+    if (n == 0 or currNum[0] == '0'):
+        return oldName
+          
+    # Found a likely looking number
+    # tokens now possess' the remaining title
+    # a_token now has the original and possible multiple iteration number
+    # such as 166b
+    i = re.findall(r'\S*' + re.escape(currNum) + '(\S*)$', a_token)
+    newNum = ' ' + '0'*(4-n) + currNum + (i.pop() if (len(i) > 0) else ' ') + ' '
+
+    # Construct new title
+    newTitle = 'Challenge' + newNum + difficulty.capitalize() + ' - '
+
+    suffix = " ".join([w for w in tokens
+        if len(re.findall(r'' +
+                      '\[' + difficulty + '|' +
+                      currNum + '|' +
+                     'challenge',
+                     w, re.IGNORECASE)) == 0])
+
+    return newTitle + suffix
+
+
+def main():
+
+    levels = ['easy', 'intermediate', 'hard']
+
+    for level in levels:
+        rawChallengeNames = glob.glob(os.path.join(level + ' Challenges/*'))
+
+        numCharsToRemove = len(level + ' Challenges/')
+
+        # remove the directory from the name
+        challengeNames = list(map(lambda x: x[numCharsToRemove:],
+                                  rawChallengeNames))
+
+        parent_dir = level.capitalize() + ' Challenges/'
+
+        for each in challengeNames:
+            if each != update_challenge_dir_name(each, level):
+                os.renames(parent_dir + each,
+                           parent_dir + update_challenge_dir_name(each, level))
+               #  print(each + ' <::> ' + update_challenge_dir_name(each, level))
+        print(level.capitalize() + ' Challenges!')
+    return
+
+if __name__ == '__main__':
+    main()
